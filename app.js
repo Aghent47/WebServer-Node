2401fd530d6110247cdb837997a6028b9d3580a7
Neider Hernández <neiderkk@gmail.com>
2022-08-12 15:03:18 -0500

    Servir contenido estático

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const app = express()
 const port = 3000;
 
-app.get('/',  (req, res) =>{
-  res.send('Home Page')
-});
+// Middleware
+
+app.use(express.static('public'));
 
 app.get('/hello-world', (req, res) => {
     res.send('Hello World!!')
 });
 
 app.get('*', (req, res) => {
-    res.send('404 Page Not Found')
+    res.sendFile( __dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
